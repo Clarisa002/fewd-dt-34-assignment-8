1da59d538f376b6dd61163f58ec74cfa657cf286
Clarisa <Clarisa002@github.com>
2018-04-12 19:06:43 -0700

    final

---
+++ starter_code/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+function myFunction() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
